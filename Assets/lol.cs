f3a5a219c9753a13ce2708430a994d1093bee4ad
Feng Lin <1693254@CEGEPGIM.LCL>
2017-07-11 15:58:45 -0400

    5

---
+++ Assets/lol.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lol : MonoBehaviour {
+
+    public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player);
+        transform.Translate(Vector3.forward * Time.deltaTime);
+	
+	}
+}
